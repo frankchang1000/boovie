3f7e4f3ffc651a5bdcc033666f93d37889d55cf8
Vineeth Sendilraj <vineethsendilraj@gmail.com>
2024-10-26 12:12:25 -0400

    pushing some changs

--- react-flask-app/src/App.jsx
+++ react-flask-app/src/App.jsx
@@ -1,11 +1,16 @@
 import "@mantine/core/styles.css";
 import { useState } from "react";
 import "./App.css";
-import { createTheme, MantineProvider } from "@mantine/core";
+import {
+  createTheme,
+  MantineProvider,
+  ColorSchemeProvider,
+  Title,
+  Text,
+} from "@mantine/core";
 import { DropzoneButton } from "../components/upload";
 import { CardsCarousel } from "../components/carousel";
 import { ActionToggle } from "../components/themeselector";
-
 import Slider from "../components/slider";
 
 const theme = createTheme({
@@ -13,17 +18,40 @@ const theme = createTheme({
 });
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [colorScheme, setColorScheme] = useState("light");
+
+  const toggleColorScheme = (value) => {
+    const nextColorScheme =
+      value || (colorScheme === "dark" ? "light" : "dark");
+    setColorScheme(nextColorScheme);
+    document.documentElement.setAttribute("data-theme", nextColorScheme);
+  };
 
   return (
-    <>
-      <MantineProvider theme={theme}>
-        <Slider />
-        <CardsCarousel />
-        <DropzoneButton />
-        <ActionToggle />
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider theme={theme} colorScheme={colorScheme}>
+        <div className="app-container">
+          <Title order={1} size="h1" style={{ marginBottom: "2rem" }}>
+            Preview Your Next Great Read in an{" "}
+            <Text
+              component="span"
+              inherit
+              color="blue"
+              style={{ display: "inline" }}
+            >
+              Immersive Trailer
+            </Text>
+          </Title>
+          <Slider />
+          <CardsCarousel />
+          <DropzoneButton />
+          <ActionToggle />
+        </div>
       </MantineProvider>
-    </>
+    </ColorSchemeProvider>
   );
 }
 
