80a74517c3e5cc80da3a42a6fc9ce27daea80154
Alex Chen <alexchen@lawn-143-215-117-122.lawn.gatech.edu>
2024-10-25 22:44:27 -0400

    carousel component

--- app.py
+++ app.py
@@ -31,7 +31,10 @@ def update_index(direction):
 
 current_option = bookOptions[st.session_state.current_index]
 
+st.image(current_option, caption=f"Book {st.session_state.current_index + 1}")
 
+if st.button(f"Select Book {st.session_state.current_index + 1}"):
+    st.success(f"You selected Book {st.session_state.current_index + 1}!") #completes action after book has been selected
 
 col1, col2 = st.columns(2)
 
@@ -42,35 +45,20 @@ with col2:
     if st.button('Next'):
         update_index('right')
 
-st.image(current_option, caption=f"Book {st.session_state.current_index + 1}")
-
-if st.button(f"Select Book {st.session_state.current_index + 1}"):
-    st.success(f"You selected Book {st.session_state.current_index + 1}!") #completes action after book has been selected
-
-
-st.write(f"Current Book Index: {st.session_state.current_index + 1}/{len(bookOptions)}")
 
 st.write("\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
+videoDisplay = st.Page("videoDisplay.py", title="Create entry", icon=":material/add_circle:")
+
+pg = st.navigation([create_page, delete_page])
 
 with st.form("my_form"):
     st.write("Create your own video!")
     script = st.text_input("Book script")
-    st.form_submit_button()
+    submit = st.form_submit_button(label = "Submit")
 
-st.write(script)
+st.write(script, "\n\n")
 
-def progressBar():
-    st.write('Starting')
-    latest_iteration = st.empty()
-    bar = st.progress(0)
 
-    for i in range(100):
-        latest_iteration.text(f'{i + 1}%')
-        bar.progress(i + 1)
-        time.sleep(0.05)
-
-    st.write('Done downloading')
-
-if(st.button("Start download")):
-    progressBar()
+if st.button("video display"):
+    st.switch_page("videoDisplay")
