3a0f56fff672d4e038b80629b89a7cf6ceb8003e
Vineeth Sendilraj <vineethsendilraj@gmail.com>
2024-10-26 17:58:55 -0400

    added logo

--- react-flask-app/components/navbar.tsx
+++ react-flask-app/components/navbar.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Container, Group, Burger } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { MantineLogo } from "@mantinex/mantine-logo";
 import classes from "../css/navbar.module.css";
 import { ActionToggle } from "./themeselector";
+import Logo from "../imgs/logo.svg";
 
 export default function NavBar({ themeMode, setThemeMode }) {
   const [opened, { toggle }] = useDisclosure(false);
@@ -11,7 +11,10 @@ export default function NavBar({ themeMode, setThemeMode }) {
   return (
     <header className={classes.header}>
       <Container size="md" className={classes.inner}>
-        <MantineLogo size={28} />
+        <div className={classes.logoContainer}>
+          <img src={Logo} alt="Logo" width={50} height={50} />
+          <h3>Boovies</h3>
+        </div>
         <Group>
           <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
           <ActionToggle theme={themeMode} setTheme={setThemeMode} />
