575ea26844e9c161fe6facf02f58fc2fea4becc3
Vineeth Sendilraj <vineethsendilraj@gmail.com>
2024-10-25 23:12:45 -0400

    pushing stuff

--- react-flask-app/src/App.jsx
+++ react-flask-app/src/App.jsx
@@ -6,6 +6,8 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { createTheme, MantineProvider } from "@mantine/core";
 
+import { CardsCarousel } from "../components/carousel";
+
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
@@ -16,26 +18,7 @@ function App() {
   return (
     <>
       <MantineProvider theme={theme}>
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
+        <CardsCarousel />
       </MantineProvider>
     </>
   );
